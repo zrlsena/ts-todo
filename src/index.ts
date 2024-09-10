12d65d322a14ad9f65668bc1f4809f119b999c7e
Sena Zorlu <senazorlux@gmail.com>
2024-09-10 13:00:03 +0300

    final

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,93 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from "uuid"
+import "./styles.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import * as bootstrap from 'bootstrap';
 
-import confetti from 'canvas-confetti';
+type Task = {
+  id: string
+  title: string
+  completed: boolean
+  createdAt: Date
+}
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.getElementById("new-task-form") as HTMLFormElement | null
+const errorModalElement = document.getElementById("errorModal") as HTMLElement;
+const errorModal = new bootstrap.Modal(errorModalElement);
+
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
+const tasks: Task[] = loadTasks()
+tasks.forEach(addListItem)
+
+form?.addEventListener("submit", e => {
+  e.preventDefault()
+
+  if (input?.value == "" || input?.value == null) return
+
+  if (isDuplicate(input?.value)) {
+    errorModal.show();
+    return;
+  }
+
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  }
+  addListItem(newTask)
+  input.value = ""
+  
+  tasks.push(newTask)
+  saveTasks()
+})
+
+function isDuplicate(title: string): boolean {
+  return tasks.some(task => task.title.toLowerCase() === title.toLowerCase())
+}
+
+function addListItem(task: Task) {
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  const deleteButton = document.createElement("button")
+
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked
+    saveTasks()
+  })
+
+  checkbox.type = "checkbox"
+  checkbox.checked = task.completed
+
+  deleteButton.textContent = "x"
+  deleteButton.classList.add("delete-button")
+  deleteButton.addEventListener("click", () => {
+    item.remove()
+
+    const taskIndex = tasks.findIndex(t => t.id === task.id)
+    if (taskIndex !== -1) {
+      tasks.splice(taskIndex, 1)
+      saveTasks()
+    }
+  })
+
+  label.append(checkbox, task.title)
+  item.append(label, deleteButton)
+  list?.append(item)
+
+  if (list) {
+    list.insertBefore(item, list.firstChild);
+  }
+}
+
+function saveTasks() {
+  localStorage.setItem("TASKS", JSON.stringify(tasks))
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem("TASKS")
+  if (taskJSON == null) return []
+  return JSON.parse(taskJSON)
+}
